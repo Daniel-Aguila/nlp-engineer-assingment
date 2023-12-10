8c0e3cec54f5f7cfde97ba943c4ac8f2a4756953
Daniel Aguila <danielbaguila@gmail.com>
2023-12-10 15:59:50 -0600

    Dec 10: Implemented Scaled Dot-Product Attention layer

--- src/nlp_engineer_assignment/transformer.py
+++ src/nlp_engineer_assignment/transformer.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+from nlp_engineer_assignment.utils import score
+
 
 class Transformer(nn.Module):
     """
@@ -19,7 +21,9 @@ class Transformer(nn.Module):
         #having to create a separate tensor
         max_sequence_length = embeddings.shape[1]
         dimensions = embeddings.shape[2]
-        positional_embeddings = embeddings.clone()
+        positional_embeddings = embeddings.clone() #to keep names meaningful
+        #TODO Study Pytorch to implement a more optimized way to apply the equations without a double for loop.
+        #This is really slow with long sequences and high dimensions
         for position in range(max_sequence_length):
             for i in range(dimensions):
                 if i % 2 == 0:
@@ -28,6 +32,19 @@ class Transformer(nn.Module):
                     positional_embeddings[:,position,i] += torch.cos(position / (10000**(2*i/dimensions))) #for odd dimensions
         return positional_embeddings
 
+    def ScaledDotProductAttention(self,query,key,value,key_dimension=512):
+        
+        key_transposed = torch.transpose(key,1,2) #Transposed the Key_dimension
+        scores = torch.matmul(query,key_transposed) #Perform the the MatMul layer from the paper to create the scores
+        scaled_scores = scores/(key_dimension**0.5) #Scale down the scores by a sqrt of the dimension of key
+        attention_weights = torch.nn.Softmax(scaled_scores) #Apply Softmax layer
+        attention_output = torch.matmul(attention_weights,value) #Final MatMul layer between the Value and Final product between Query and Key
+        
+        return attention_output
+
+    def MultiHeadAttention(self):
+        #TODO Implement parallelism of the ScaledDotProductAttention layers
+        return
 
 def train_classifier(train_inputs):
     # TODO: Implement the training loop for the Transformer model.
