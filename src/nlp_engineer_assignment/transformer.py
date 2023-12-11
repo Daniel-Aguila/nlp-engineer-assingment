177d6fcb15b922f6694110cd7b6ba1c1c0f9e1a0
Daniel Aguila <danielbaguila@gmail.com>
2023-12-11 04:18:19 -0600

    Dec 11: Model forward function, as well as the feed forward block were implemented - Need to add Add & Norm, changed dkey to 512 since it won't split into different heads at the moment

--- src/nlp_engineer_assignment/transformer.py
+++ src/nlp_engineer_assignment/transformer.py
@@ -1,3 +1,4 @@
+from turtle import forward
 import torch
 import torch.nn as nn
 
@@ -11,10 +12,18 @@ class Transformer(nn.Module):
     DO NOT use pre-implemented layers for the architecture, positional encoding or self-attention,
     such as nn.TransformerEncoderLayer, nn.TransformerEncoder, nn.MultiheadAttention, etc.
     """
-    def __init__(self,input_size,output_dimension=512):
+    def __init__(self,input_size,output_dimension=512,inner_layer=2048):
         super(Transformer,self).__init__()
         #This facilitates the residual connection. All of the model sub-layers and embedding layers produce outputs of dimension dmodel = 512
         self.embedding = nn.Embedding(num_embeddings=input_size, embedding_dim=output_dimension)
+        self.v_linear = nn.Linear(in_features=output_dimension,out_features=output_dimension)
+        self.k_linear = nn.Linear(in_features=output_dimension,out_features=output_dimension)
+        self.q_linear = nn.Linear(in_features=output_dimension,out_features=output_dimension)
+        self.final_self_attention_linear = nn.Linear(in_features=output_dimension,out_features=output_dimension)
+
+        self.linear1 = nn.Linear(in_features=input_size,out_features=inner_layer)
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(in_features=inner_layer,out_features=output_dimension)
 
     def PositionalEncoding(self,embeddings):
         #Since we are adding Positional Encoding to the embeddings, I figure I can just iterate through the embeddings themselves, without
@@ -32,19 +41,37 @@ class Transformer(nn.Module):
                     positional_embeddings[:,position,i] += torch.cos(position / (10000**(2*i/dimensions))) #for odd dimensions
         return positional_embeddings
 
-    def ScaledDotProductAttention(self,query,key,value,dkey=64):
+    def ScaledDotProductAttention(self,query,key,value,dkey=512):
 
         key_transposed = torch.transpose(key,1,2) #Transposed the Key_dimension
         scores = torch.matmul(query,key_transposed) #Perform the the MatMul layer from the paper to create the scores
         scaled_scores = scores/(dkey**0.5) #Scale down the scores by a sqrt of the dimension of key
-        attention_weights = torch.nn.Softmax(scaled_scores) #Apply Softmax layer
+        attention_weights = torch.nn.Softmax(dim=-1)(scaled_scores) #Apply Softmax to the sequence dimension
         attention_output = torch.matmul(attention_weights,value) #Final MatMul layer between the Value and Final product between Query and Key
         
         return attention_output
 
-    def MultiHeadAttention(self):
-        #TODO Implement parallelism of the ScaledDotProductAttention layers
-        return
+    #TODO Implement Multi-Head if there is available time and if model does not perform as well
+
+    def forward(self,x):
+        #positional embedding
+        x = self.embedding(x)
+        x = self.PositionalEncoding(x)
+
+        #self attetion, single head
+        qx = self.q_linear(x)
+        kx = self.k_linear(x)
+        vx = self.v_linear(x)
+
+        x = self.ScaledDotProductAttention(qx,kx,vx)
+        x = self.final_self_attention_linear(x)
+
+        #feed forward block
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.linear2(x)
+
+        return x
 
 def train_classifier(train_inputs):
     # TODO: Implement the training loop for the Transformer model.
