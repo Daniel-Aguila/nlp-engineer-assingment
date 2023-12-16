Dec 15: Added unit tests for the api, and preprocessingtokenize functions, testing connection, correct response, and proper conversion from character to numerical value

src/nlp_engineer_assignment/api.py
@@ -5,7 +5,7 @@ from nlp_engineer_assignment import model_predict
 import main
 from pydantic import BaseModel
 
-model = torch.load("/Users/daguila/nlp-engineer-assignment/model_97.8.pth")
+model = torch.load("model_97.8.pth")
 
 
 app = FastAPI(

tests/test_utils.py
@@ -1,7 +1,15 @@
+import json
+from urllib import request
 import numpy as np
-
+from main import tokenize
+from nlp_engineer_assignment.api import getPredictionFromText
+import pytest
 from nlp_engineer_assignment.utils import count_letters, score
+from fastapi.testclient import TestClient
+from nlp_engineer_assignment import api
 
+
+client = TestClient(api.app)
 
 def test_count_letters():
     assert np.array_equal(count_letters("hello"), np.array([0, 0, 0, 1, 0]))
@@ -21,3 +29,32 @@ def test_score():
                  np.array([[0, 0, 0, 0, 0]])) == 0.4
     assert score(np.array([[0, 1, 1, 0, 1]]),
                  np.array([[1, 0, 0, 1, 0]])) == 0.0
+
+def test_tokenize_for_incorrect_character():
+    vocab = [chr(ord('a') + i) for i in range(0, 26)] + [' ']
+    sequence = "hello!"
+    with pytest.raises(ValueError):
+        tokenize(sequence,vocab)
+
+def test_tokenize_for_character_to_number_conversion():
+    vocab = [chr(ord('a') + i) for i in range(0, 26)] + [' ']
+    sequence = "this is a test"
+    assert tokenize(sequence,vocab) == [19,7,8,18,26,8,18,26,0,26,19,4,18,19]
+
+def test_getPredictionFromText_correct_response_given_request():
+    request = {
+        "text": "yaraku is a japanese"
+    }
+    prediction = {
+        "prediction": "00010000012202020011"
+    }
+    response = client.post("/predictText", json=request)
+    assert response.json() == prediction
+
+def test_getPredictionFromText_status_code():
+    request = {
+        "text": "yaraku is a japanese"
+    }
+    response = client.post("/predictText", json=request)
+    assert response.status_code == 200
+